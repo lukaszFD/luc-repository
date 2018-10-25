bdd4c4f5d8780857045af4f322fdc437317e8840
Łukasz <lukasz.f.d@gmail.com>
2018-10-25 21:40:28 +0200

    Solved exercise (1,2,3,4,6)

---
+++ New programming technologies in C Sharp/Cwiczenia_1/Cwiczenia_1/CLasses/Boss.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_1.CLasses
+{
+    class Boss
+    {
+        public void Increase(Worker w, int newSalary)
+        {
+            w.Salary += newSalary;
+        }
+    }
+}
