4b64e8fa625cdad9c1ff732f00731dd4e27d72fe
lukaszFD <lukasz.f.d@gmail.com>
2018-10-26 13:12:11 +0200

    New ORM( C# - Entity Framework)

--- WPF_ParkingApp/Parking/Class/Days.cs
+++ WPF_ParkingApp/Parking/Class/Days.cs
@@ -8,10 +8,10 @@ namespace Parking.Class
 {
     class Days
     {
-        public List<DateTime> Weekend()
+        public List<DateTime> Weekend(int d)
         {
             List<DateTime> list = new List<DateTime>();
-            TimeSpan diff = DateTime.Now.AddMonths(6) - DateTime.Now;
+            TimeSpan diff = DateTime.Now.AddDays(d) - DateTime.Now; 
             int days = diff.Days;
             for (var i = 0; i <= days; i++)
             {
@@ -20,6 +20,28 @@ namespace Parking.Class
                 {
                     case DayOfWeek.Saturday:
                     case DayOfWeek.Sunday:
+                        list.Add(date);
+                        break;
+                }
+            }
+            return list;
+        }
+
+        public List<DateTime> WorkingDays(DateTime dateFrom, DateTime dateTo)
+        {
+            List<DateTime> list = new List<DateTime>();
+            TimeSpan diff = dateTo - dateFrom;
+            int days = diff.Days;
+            for (var i = 0; i <= days; i++)
+            {
+                var date = DateTime.Now.AddDays(i);
+                switch (date.DayOfWeek)
+                {
+                    case DayOfWeek.Monday:
+                    case DayOfWeek.Tuesday:
+                    case DayOfWeek.Wednesday:
+                    case DayOfWeek.Thursday:
+                    case DayOfWeek.Friday:
                         list.Add(date);
                         break;
                 }
