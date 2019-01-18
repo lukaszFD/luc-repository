6a7fa394fe9b29f32630f91b5286c05830e81eb5
lukaszFD <lukasz.f.d@gmail.com>
2019-01-18 22:25:59 +0100

    new

--- IPI_PAN_PROGRAMOWANIE _NA_PLATFORMIE_NET/New_Tech_CS/Exam/Exam_Hotel/Program.cs
+++ IPI_PAN_PROGRAMOWANIE _NA_PLATFORMIE_NET/New_Tech_CS/Exam/Exam_Hotel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,154 @@ namespace Exam_Hotel
     {
         static void Main(string[] args)
         {
+            Pokoj p = new Pokoj();
+
+            //p.Zarezerwuj(5, "Lipa");
+            //p.Wycofaj(1, "Dejko");
+            p.Wydaj(10, "Mały");
+
+            Console.WriteLine(p);
+
+            foreach (var item in Hotel.pokojeHotelowe)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Metody
+    {
+        abstract public void WyszukajWolnyPokoj();
+        abstract public void ZarezerwujPokoj();
+    }
+
+    class Pokoj
+    {
+        public string Nazwisko { get; set; }
+        public int NumerPokoju { get; set; }
+        public Stan StanPokoju { get; set; }
+
+        public enum Stan
+        {
+            wolny, 
+            zajety, 
+            zarezerwowany,
+            wycofany,
+        }
+        public Pokoj() { }
+
+        public Pokoj(int numer, string nazwisko, Stan stan)
+        {
+            this.NumerPokoju = numer;
+            this.Nazwisko = nazwisko;
+            this.StanPokoju = stan;
+        }
+
+        public void Zarezerwuj(int numerek, string nazwisko)
+        {
+            Hotel.pokojeHotelowe.Enqueue(new Pokoj(numerek, nazwisko, Stan.zarezerwowany));
+        }
+
+        public void Wycofaj(int numerek, string nazwisko)
+        {
+            Pokoj pok = Hotel.pokojeHotelowe.Dequeue();
+
+            pok.NumerPokoju = numerek;
+            pok.Nazwisko = nazwisko;
+            pok.StanPokoju = Stan.wycofany;
+        }
+        public void Przyjmij(int numerek, string nazwisko)
+        {
+            Pokoj pok = Hotel.pokojeHotelowe.Dequeue();
+
+            pok.NumerPokoju = numerek;
+            pok.Nazwisko = nazwisko;
+            pok.StanPokoju = Stan.wolny;
+        }
+        public void Wydaj(int numerek, string nazwisko)
+        {
+            foreach (var item in Hotel.pokojeHotelowe)
+            {
+                if (item.StanPokoju == Pokoj.Stan.wolny && item.NumerPokoju == numerek)
+                {
+                    Hotel.pokojeHotelowe.Enqueue(new Pokoj(numerek, nazwisko, Stan.zarezerwowany));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Klient : {0} , Numer pokoju : {1}, Status : {2}",Nazwisko,NumerPokoju, StanPokoju);
+        }
+    }
+    class Gosc : Metody
+    {
+        string Nazwisko { get; set; }
+
+        public override void WyszukajWolnyPokoj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ZarezerwujPokoj()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Hotel
+    {
+        public static Queue<Pokoj> pokojeHotelowe { get; set; }
+
+        static Hotel()
+        {
+            pokojeHotelowe = new Queue<Pokoj>();
+            pokojeHotelowe.Enqueue(new Pokoj(1, "Dejko", Pokoj.Stan.zarezerwowany));
+            pokojeHotelowe.Enqueue(new Pokoj(2, "Kowalski", Pokoj.Stan.zarezerwowany));
+            pokojeHotelowe.Enqueue(new Pokoj(10, "Kostek", Pokoj.Stan.wolny));
+        }
+        //dodawanie Queue.enqueue(element)
+        //usuwanie Queue.dequeue()
+        //spr ile Queue.Count
+        //czy zawiera Queue.Contains(element)
+        //wywaleanie z kolejki Queue.Clear
+    }
+
+    class DyrektorHotelu
+    {
+        void DodajPokoj()
+        {
+
+        }
+
+        void WycofajPokoj()
+        {
+
+        }
+    }
+
+    class Recepcjonista : Metody
+    {
+        public override void WyszukajWolnyPokoj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ZarezerwujPokoj()
+        {
+            throw new NotImplementedException();
+        }
+
+        void WyszukajZarezerwowany()
+        {
+
+        }
+
+        void ZwolnijPokoj()
+        {
+
         }
     }
 }
---
+++ IPI_PAN_PROGRAMOWANIE _NA_PLATFORMIE_NET/New_Tech_CS/Exam/Hotel_Iza/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hotel_Iza
+{
+    public class Pokoj
+    {
+        public int NrPokoju { get; set; }
+        public stanPokoju AktualnyStan { get; set; }
+        public string NazwiskoGoscia { get; set; }
+        public enum stanPokoju { wolny, zajety, zarezerwowany, wycofany };
+
+        public Pokoj(int NrPokoju, stanPokoju AktualnyStan, string NazwiskoGoscia)
+        {
+            this.NrPokoju = NrPokoju;
+            this.AktualnyStan = AktualnyStan;
+            this.NazwiskoGoscia = NazwiskoGoscia;
+        }
+
+        public void Zarezerwuj(int NrPokoju, string nazwisko)
+        {
+            AktualnyStan = stanPokoju.wolny;
+            NazwiskoGoscia = nazwisko;
+        }
+        public void Wydaj(int NrPokoju, string nazwisko)
+        {
+            AktualnyStan = stanPokoju.zajety;
+            NazwiskoGoscia = nazwisko;
+        }
+        public void Przyjmij(int NrPokoju, string nazwisko)
+        {
+            AktualnyStan = stanPokoju.wolny;
+            NazwiskoGoscia = nazwisko;
+        }
+        public void Wycofaj(int NrPokoju, string nazwisko)
+        {
+            AktualnyStan = stanPokoju.wycofany;
+            NazwiskoGoscia = nazwisko;
+        }
+
+        public override String ToString()
+        {
+            if (AktualnyStan == stanPokoju.zajety || AktualnyStan == stanPokoju.zarezerwowany)
+            {
+                return "Stan pokoju numer " + NrPokoju + " to " + AktualnyStan + ". Gość o nazwisku " + NazwiskoGoscia;
+            }
+            else
+            {
+                return "Stan pokoju numer " + NrPokoju + " to " + AktualnyStan;
+            }
+        }
+    }
+
+    public abstract class ObslugaPokoju
+    {
+        public abstract int WyszukajWolnyPokoj();
+        public abstract bool ZarezerwujPokoj(int nrpokoju, string nazwisko);
+
+    }
+    public class Gosc : ObslugaPokoju
+    {
+        public override int WyszukajWolnyPokoj()
+        {
+            List<Pokoj> listaWolnych = new List<Pokoj>();
+            foreach (var pokoj in Hotel.Pokoje)
+            {
+                if (pokoj.AktualnyStan == Pokoj.stanPokoju.wolny)
+                {
+                    listaWolnych.Add(pokoj);
+                }
+            }
+            return listaWolnych.First().NrPokoju;
+        }
+        public override bool ZarezerwujPokoj(int nr, string nazwisko)
+        {
+            bool flaga = false;
+            foreach (var item in Hotel.Pokoje)
+            {
+                if (item.NrPokoju == nr && item.AktualnyStan == Pokoj.stanPokoju.wolny)
+                {
+                    item.AktualnyStan = Pokoj.stanPokoju.zarezerwowany;
+                    item.NazwiskoGoscia = nazwisko;
+                    flaga = true;
+                }
+            }
+            if (flaga == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Hotel
+    {
+        public static Queue<Pokoj> Pokoje { get; set; }
+        static Hotel()  // to jest konstruktor, inna nazwa nie gra
+        {
+            Pokoje = new Queue<Pokoj>();
+            Pokoje.Enqueue(new Pokoj(1, Pokoj.stanPokoju.wolny, "Kowalski"));
+            Pokoje.Enqueue(new Pokoj(2, Pokoj.stanPokoju.zajety, "Michni"));
+            Pokoje.Enqueue(new Pokoj(3, Pokoj.stanPokoju.wolny, "Kwit"));
+            Pokoje.Enqueue(new Pokoj(4, Pokoj.stanPokoju.zarezerwowany, "Koleba"));
+            Pokoje.Enqueue(new Pokoj(5, Pokoj.stanPokoju.wolny, "Ktory"));
+            Pokoje.Enqueue(new Pokoj(6, Pokoj.stanPokoju.zajety, "Krok"));
+        }
+    }
+
+    public class DyrektorHotelu
+    {
+        public bool DodajPokoj(Pokoj pokoj)
+        { return true; }
+        public bool WycofajPokoj(Pokoj pokoj)
+        { return true; }
+
+    }
+    public class Recepcjonista : ObslugaPokoju
+    {
+        public override int WyszukajWolnyPokoj()
+        {
+            List<Pokoj> listaWolnych = new List<Pokoj>();
+            foreach (var pokoj in Hotel.Pokoje)
+            {
+                if (pokoj.AktualnyStan == Pokoj.stanPokoju.wolny)
+                {
+                    listaWolnych.Add(pokoj);
+                }
+            }
+            return listaWolnych.First().NrPokoju;
+        }
+        public override bool ZarezerwujPokoj(int nr, string nazwisko)
+        {
+            bool flaga = false;
+            foreach (var item in Hotel.Pokoje)
+            {
+                if (item.NrPokoju == nr && item.AktualnyStan == Pokoj.stanPokoju.wolny)
+                {
+                    item.AktualnyStan = Pokoj.stanPokoju.zarezerwowany;
+                    item.NazwiskoGoscia = nazwisko;
+                    flaga = true;
+                }
+            }
+            if (flaga == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int WyszukajZarezerwowany()
+        {
+            List<Pokoj> listazarezerwowanych = new List<Pokoj>();
+            foreach (var pokoj in Hotel.Pokoje)
+            {
+                if (pokoj.AktualnyStan == Pokoj.stanPokoju.zarezerwowany)
+                {
+                    listazarezerwowanych.Add(pokoj);
+                }
+            }
+            return listazarezerwowanych.First().NrPokoju;
+        }
+
+        public bool ZwolnijPokoj(int nr)
+        {
+            bool flaga = false;
+            foreach (var item in Hotel.Pokoje)
+            {
+                if (item.NrPokoju == nr && item.AktualnyStan == Pokoj.stanPokoju.zajety)
+                {
+                    item.AktualnyStan = Pokoj.stanPokoju.wolny;
+                    item.NazwiskoGoscia = "";
+                    flaga = true;
+                }
+            }
+            if (flaga == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel.Pokoje.Enqueue(new Pokoj(126, Pokoj.stanPokoju.zarezerwowany, "ZpoziomuProgramu"));
+
+            foreach (var item in Hotel.Pokoje)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Gosc gosc = new Gosc();
+            int a = gosc.WyszukajWolnyPokoj();
+            Console.WriteLine("Wolny pokoj to " + a);
+            gosc.ZarezerwujPokoj(a, "Milosz");
+            foreach (var item in Hotel.Pokoje)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
